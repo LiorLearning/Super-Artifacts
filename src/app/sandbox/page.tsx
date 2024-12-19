3a731df176086f05199cf1459ee8115df7cdd2ce
Sahasra Ranjan <sahasraranjan@gmail.com>
2024-12-19 21:20:03 +0530

    build fixes

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -2,13 +2,16 @@
 
 import { MessageProvider } from '@/components/MessageContext';
 import { WebSocketProvider } from '@/components/websocket';
+import { Suspense } from 'react';
 import SandboxPage from './sandbox';
 
 export default function Page() {
   return (
     <MessageProvider>
         <WebSocketProvider url={`${process.env.NEXT_PUBLIC_WS_BASE_URL}/superartifacts/ws`}>
-            <SandboxPage />
+            <Suspense fallback={<div>Loading...</div>}>
+                <SandboxPage />
+            </Suspense>
         </WebSocketProvider>
     </MessageProvider>
   )
