b76f16553c77a4613a7d09f160497a67d5066670
Sahasra Ranjan <sahasraranjan@gmail.com>
2024-12-30 15:14:41 +0530

    addn game working

--- src/components/artifacts/gameMaster.tsx
+++ src/components/artifacts/gameMaster.tsx
@@ -11,6 +11,7 @@ import { handleScreenshot } from './utils/utils';
 
 import FractionAdditionGame from './games/fraction-addition/game';
 import AdditionGame from './games/addition/game';
+<<<<<<< HEAD
 import FractionsGame from './games/fractions-game/game';
 import EquivalentFractionsGame from './games/equivalent-fractions/game';
 
@@ -23,6 +24,15 @@ import { desc as FractionAdditionGameDesc } from './games/fraction-addition/game
 import { desc as AdditionGameDesc } from './games/addition/game-state';
 import { desc as FractionsGameDesc } from './games/fractions-game/game-state';
 import { desc as EquivalentFractionsGameDesc } from './games/equivalent-fractions/game-state';
+=======
+
+import { GameStateProvider as FractionAdditionGameStateProvider, useGameState as FractionAdditionGameState } from './games/fraction-addition/state-utils'
+import { GameStateProvider as AdditionGameStateProvider, useGameState as AdditionGameState } from './games/addition/state-utils'
+
+import { desc as FractionAdditionGameDesc } from './games/fraction-addition/game-state';
+import { desc as AdditionGameDesc } from './games/addition/game-state';
+
+>>>>>>> 2ea4a60 (addn game working)
 
 
 interface GameInfo {
@@ -41,6 +51,7 @@ const gameInfo: Record<string, GameInfo> = {
     state: AdditionGameState,
     provider: AdditionGameStateProvider
   },
+<<<<<<< HEAD
   'fractions-game': {
     game: FractionsGame,
     desc: FractionsGameDesc,
@@ -53,6 +64,20 @@ const gameInfo: Record<string, GameInfo> = {
     state: EquivalentFractionsGameState,
     provider: EquivalentFractionsGameStateProvider
   },
+=======
+  // 'fractions-game': {
+  //   game: FractionsGame,
+  //   desc: FractionsGameDesc,
+  //   state: FractionsGameState,
+  //   provider: FractionsGameStateProvider
+  // },
+  // 'equivalent-fractions-game': {
+  //   game: EquivalentFractionsGame,
+  //   desc: EquivalentFractionsGameDesc,
+  //   state: EquivalentFractionsGameState,
+  //   provider: EquivalentFractionsGameStateProvider
+  // },
+>>>>>>> 2ea4a60 (addn game working)
   'fraction-addition': {
     game: FractionAdditionGame,
     desc: FractionAdditionGameDesc,
