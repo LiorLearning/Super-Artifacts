1f9bcf13170631a9291c833ced5c58a4c4fec847
deepak silaych <deepaksilaych@gmail.com>
2025-01-02 21:58:40 +0530

    updated addition with second part

--- src/components/artifacts/games/addition/game.tsx
+++ src/components/artifacts/games/addition/game.tsx
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React, { useState } from 'react'
 import First from './screenOne'
 import Second from './screenTwo';
 
@@ -7,8 +6,8 @@ interface GameProps {
   sendAdminMessage: (role: string, content: string) => void;
 }
 
-function game({ sendAdminMessage }: GameProps) {
-  const [firstDone, setFirstDone] = React.useState(true)
+function Game({ sendAdminMessage }: GameProps) {
+  const [firstDone, setFirstDone] = useState<boolean>(false);
   return (
     <div className="h-full w-full bg-white">
       <div className="flex flex-col pb-16 h-full w-full justify-center items-center font-gaegu bg-[linear-gradient(90deg,rgba(0,0,0,.1)_1px,transparent_1px),linear-gradient(rgba(0,0,0,.1)_1px,transparent_1px)] bg-[length:20px_20px]">
@@ -17,16 +16,15 @@ function game({ sendAdminMessage }: GameProps) {
           :
           <Second sendAdminMessage={sendAdminMessage} />
         }
-      <style jsx global>{`
-        @import url('https://fonts.googleapis.com/css2?family=Gaegu:wght@300;400;700&display=swap');
-
-        .font-gaegu {
-          font-family: 'Gaegu', cursive;
-        }
-      `}</style>
+        <style jsx global>{`
+          @import url('https://fonts.googleapis.com/css2?family=Gaegu:wght@300;400;700&display=swap');
+          .font-gaegu {
+            font-family: 'Gaegu', cursive;
+          }
+        `}</style>
+      </div>
     </div>
-  </div>
   )
 }
 
-export default game
+export default Game
