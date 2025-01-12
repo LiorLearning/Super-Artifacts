e5a65caa51ee6e7b7190b10085da100e02d8c3dc
deepak silaych <deepaksilaych@gmail.com>
2025-01-13 02:12:17 +0530

    equivalent fraction

--- src/components/artifacts/games/equivalent-fractions/components/honeyselctor.tsx
+++ src/components/artifacts/games/equivalent-fractions/components/honeyselctor.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { sounds } from '../utils/sounds';
 
 export default function HoneySelector({options, selectedHoney, setSelectedHoney}: {options: number[], selectedHoney: number | null, setSelectedHoney: (value: number) => void}) {
   return (
     options.map((pieces) => (
       <button
         key={pieces}
-        onClick={() =>  setSelectedHoney(pieces)}
+        onClick={() => {
+          sounds.button();
+          setSelectedHoney(pieces);
+        }}
         className={`flex items-center gap-2 mt-1 px-4 py-2 rounded ${
           selectedHoney === pieces ? 'bg-red-500' : 'bg-red-200'
         }`}
