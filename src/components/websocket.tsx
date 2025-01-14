dd8862c45d71d5fb061254148441870093d108f4
Sahasra Ranjan <sahasraranjan@gmail.com>
2025-01-14 19:50:42 +0530

    proceed button

--- src/components/websocket.tsx
+++ src/components/websocket.tsx
@@ -3,7 +3,7 @@ import { MessageContext, Message, useMessageContext } from './MessageContext';
 import { WifiOff, Wifi } from 'lucide-react';
 import { AudioContext } from './utils/audio_stream';
 
-const SPEAKOUT = false;
+const SPEAKOUT = true;
 
 type WebSocketContextType = {
   sendLog: (message: Message | Blob) => void;
