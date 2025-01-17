ce43d24937bcfb668185606fc99cd7dd44398542
Sahasra Ranjan <sahasraranjan@gmail.com>
2025-01-18 03:49:27 +0530

    lego game: narration and design interations (#107)
    * lego game: narration and design interations
    
    * remove grids

--- src/components/artifacts/games/mixed-fractions-to-improper-fractions/components/choose-holder.tsx
+++ src/components/artifacts/games/mixed-fractions-to-improper-fractions/components/choose-holder.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { GameProps } from "../utils/types";
+import { useEffect, useState, useRef } from "react";
+import { COLORS } from "../screen/constants";
 
 interface ChooseHolderProps extends GameProps {
   answer: number;
@@ -8,6 +10,9 @@ interface ChooseHolderProps extends GameProps {
 }
 
 export const ChooseHolder = ({ answer, denomOptions, onSuccess, sendAdminMessage }: ChooseHolderProps) => {
+  const [showButton, setShowButton] = useState(false);
+  const hasGameStarted = useRef(false);
+
   const handleDenomOptionClick = (option: number) => {
     if (option === answer) {
       onSuccess();
@@ -17,14 +22,30 @@ export const ChooseHolder = ({ answer, denomOptions, onSuccess, sendAdminMessage
     }
   };
 
+  useEffect(() => {
+    if (!hasGameStarted.current) {
+      setTimeout(() => {
+        sendAdminMessage('agent', "A hint: Look at the denominator");
+        setShowButton(true);
+      }, 8000);
+      hasGameStarted.current = true;
+    }
+  }, []);
+
+  if (!showButton) {
+    return null;
+  }
 
   return (
     <div className="flex flex-col items-center justify-center mt-4 space-y-2">
-      <div className="flex justify-center space-x-4">
+      <div className="flex justify-center space-x-16">
         {denomOptions.map((option, index) => (
           <Button 
             key={index} 
-            className="bg-blue-500 text-white px-4 py-2 text-xl rounded-lg hover:bg-blue-600 transition-colors duration-300 shadow-md"
+            className="px-8 py-4 text-3xl rounded-none transition-colors duration-300 shadow-[-5px_5px_0px_0px_rgba(0,0,0,1)]"
+            style={{
+              backgroundColor: COLORS.pink,
+            }}
             onClick={() => handleDenomOptionClick(option)}
           >
             {option}
