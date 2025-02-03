ca41b0e36473d0c034fb60c0b17ff97c22e20adc
ayush2743 <saxenaayush27.work@gmail.com>
2025-02-04 04:04:28 +0530

    solve the screen2 error

--- src/components/artifacts/games/multiplying-wholes-and-Fractions/screen/second.tsx
+++ src/components/artifacts/games/multiplying-wholes-and-Fractions/screen/second.tsx
@@ -4,11 +4,11 @@ import { BaseProps } from '../utils/types';
 
 export default function SecondScreen({ sendAdminMessage }: BaseProps) {
   const { gameStateRef } = useGameState();
-  const { step, variable } = gameStateRef.current.state2;
+  const { step } = gameStateRef.current.state2;
 
   return (
     <div className="mx-auto">
-      <Header variable={variable} />
+
     </div>
   )
 }
