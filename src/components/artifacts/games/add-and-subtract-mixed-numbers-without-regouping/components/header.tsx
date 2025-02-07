6e2b847d4862dc6521b5faf9b5df9c3ca9b9b7cc
Deepak Silaych <69294059+DeepakSilaych@users.noreply.github.com>
2025-02-08 04:25:53 +0530

    updated font and conversations (#162)

--- src/components/artifacts/games/add-and-subtract-mixed-numbers-without-regouping/components/header.tsx
+++ src/components/artifacts/games/add-and-subtract-mixed-numbers-without-regouping/components/header.tsx
@@ -18,10 +18,10 @@ const Header: React.FC<HeaderProps> = (({ fraction1, fraction2, type, version })
           whole={fraction1.whole} 
           numerator={fraction1.numerator} 
           denominator={fraction1.denominator} 
-          className='text-3xl font-extrabold'
+          className='text-3xl'
         />
       </div>
-      <span className="text-3xl font-bold">
+      <span className="text-3xl font-bold text-white">
         {type === 'addition' ? '+' : '-'}
       </span>
       <div className='bg-white px-2 pr-4 '>
@@ -29,39 +29,39 @@ const Header: React.FC<HeaderProps> = (({ fraction1, fraction2, type, version })
           whole={fraction2.whole} 
           numerator={fraction2.numerator} 
           denominator={fraction2.denominator} 
-          className='text-3xl font-extrabold'
+          className='text-3xl'
           />
         </div>
       </div>
       {version === 2 &&
         <div className="flex items-center justify-center gap-4 my-8 py-4 bg-[#F97315] bg-opacity-20">
           <div className="flex bg-[#FFC5C6] p-2 items-stretch gap-2 border-2 border-gray-500 rounded-lg shadow-[-2px_2px_1px_rgba(0,0,0,0.7)]">
-            <span className='bg-white rounded-lg border-2 border-gray-500 flex items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
+            <span className='bg-white rounded-lg border-2 pr-2 border-gray-500 flex items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
               <MixedFraction
                 whole={fraction1.whole}
                 numerator={fraction1.numerator}
                 denominator={fraction1.denominator}
-                className='text-xl font-extrabold p-2'
+                className='text-2xl p-2'
               />
             </span>
-            <p className='text-xl font-extrabold p-3 border-2 border-gray-500 bg-white rounded-lg flex-grow flex gap-2 items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
+            <p className='text-2xl p-3 border-2 border-gray-500 bg-white rounded-lg flex-grow flex gap-2 items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
               <div className={`flex flex-col items-center justify-center p-1 rounded-full border-[1px] border-black bg-[#FFC98F]`}>
                 <div className={`w-12 h-12 bg-[#E65A5A] border-[1px] shadow-[inset_0px_0px_4px_0px_rgba(0,0,0,0.5)] border-black rounded-full`} />
               </div>
               Pepperoni Pizza
             </p>
           </div>
-          <span className="text-5xl font-bold text-black">+</span>
+          <span className="text-5xl font-bold text-[#9B4949]">+</span>
           <div className="flex bg-yellow-200 p-2 items-stretch gap-2 border-2 border-gray-500 rounded-lg shadow-[-1px_1px_0px_rgba(0,0,0,0.7)]">
-            <span className='bg-white rounded-lg border-2 border-gray-500 flex items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
+            <span className='bg-white rounded-lg border-2 pr-2 border-gray-500 flex items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
               <MixedFraction
                 whole={fraction2.whole}
                 numerator={fraction2.numerator}
                 denominator={fraction2.denominator}
-                className='text-xl font-extrabold p-2'
+                className='text-2xl p-2'
               />
             </span>
-            <p className='text-xl font-extrabold p-3 border-2 border-gray-500 bg-white rounded-lg flex-grow flex gap-2 items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
+            <p className='text-2xl p-3 border-2 border-gray-500 bg-white rounded-lg flex-grow flex gap-2 items-center shadow-[inset_-1px_1px_0px_rgba(0,0,0,0.7)]'>
               <div className={`flex flex-col items-center justify-center p-1 rounded-full border-[1px] border-black bg-[#FFC98F]`}>
                 <div className={`w-12 h-12 bg-yellow-200 border-[1px] shadow-[inset_0px_0px_4px_0px_rgba(0,0,0,0.5)] border-black rounded-full`} />
               </div>
--- src/components/artifacts/games/add-and-subtract-mixed-numbers-without-regouping/components/proceed.tsx
+++ src/components/artifacts/games/add-and-subtract-mixed-numbers-without-regouping/components/proceed.tsx
@@ -13,13 +13,13 @@ const Proceed: React.FC<ProceedProps> = ({ onComplete }) => {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4 m-8 mb-16">
-      <h2 className="text-5xl font-bold text-[#18AF48]">
+      <h2 className="text-5xl text-[#18AF48]">
         Correct Answer
       </h2>
       
       <button
         onClick={handleClick}
-        className="flex items-center gap-2 text-4xl font-bold px-12 py-4 text-black hover:opacity-90 transition-opacity"
+        className="flex items-center gap-2 text-4xl px-12 py-4 text-black hover:opacity-90 transition-opacity"
       >
         PROCEED
         <span className="text-5xl" style={{ color: '#00C853' }}>
