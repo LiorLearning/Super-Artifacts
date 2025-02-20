86ed663ce6f2daa08e0ba46171482a872ce95e14
ayush2743 <saxenaayush27.work@gmail.com>
2025-02-20 07:21:24 +0530

    multiplying console component

--- src/components/artifacts/games/multiplying-2-digits-by-1-digit-with-partial-products/screen/FirstScreen/first.tsx
+++ src/components/artifacts/games/multiplying-2-digits-by-1-digit-with-partial-products/screen/FirstScreen/first.tsx
@@ -1,3 +1,4 @@
+import MultiplyBox from '../../components/multiplybox';
 import { useGameState } from '../../state-utils';
 import { BaseProps } from '../../utils/types';
 
@@ -6,10 +7,11 @@ export default function FirstScreen({ sendAdminMessage }: BaseProps) {
   const { gameStateRef, setGameStateRef } = useGameState();
 
   return (
-    <div>
-      hello
-    </div>
-  )
+    <div className='bg-[#B9F7FF] min-h-screen flex justify-center items-center'>
 
-  
+      <div className=''>
+        <MultiplyBox number1={23} number2={4} />
+      </div>
+    </div>
+  ) 
 }
