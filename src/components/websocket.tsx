c4d1c9e4a4c6012b1992dd4a14ac7fca78ba9c64
Sahasra Ranjan <sahasraranjan@gmail.com>
2025-02-20 22:22:30 +0530

    Disable audio

--- src/components/websocket.tsx
+++ src/components/websocket.tsx
@@ -4,7 +4,7 @@ import { WifiOff, Wifi } from 'lucide-react';
 import { AudioContext } from './utils/elevenlabs_audio_stream';
 import Profile from './auth/profile';
 
-const SPEAKOUT = false;
+export const SPEAKOUT = false;
 
 type WebSocketContextType = {
   sendLog: (message: Message | Blob) => void;
