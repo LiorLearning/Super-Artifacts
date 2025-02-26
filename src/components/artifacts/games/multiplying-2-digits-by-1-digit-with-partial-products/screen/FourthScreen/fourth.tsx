2179b1d2c6f707b734daab938c569131bdaa8235
ayush2743 <saxenaayush27.work@gmail.com>
2025-02-26 06:20:19 +0530

    changed the bg for abstract

--- src/components/artifacts/games/multiplying-2-digits-by-1-digit-with-partial-products/screen/FourthScreen/fourth.tsx
+++ src/components/artifacts/games/multiplying-2-digits-by-1-digit-with-partial-products/screen/FourthScreen/fourth.tsx
@@ -37,8 +37,10 @@ export default function FourthScreen({ sendAdminMessage }: BaseProps) {
   }, []);
 
   return (
-    <div className="min-h-screen overflow-hidden flex flex-col items-center gap-[2vh] justify-center" style={{ backgroundImage: `url(${images.practiceBg})`, backgroundSize: '100% 100%', backgroundPosition: 'center' }}>
-
+    <div className="min-h-screen bg-[#B9F7FF] overflow-hidden flex flex-col items-center justify-end">
+      <div className="absolute w-full h-[25vh] "
+        style={{ backgroundImage: `url(${images.grass})`, backgroundSize: '100% 100%' }}>
+      </div>
       {showPopUp && <div className="fixed inset-0 bg-black/60 z-50 flex items-center justify-center w-[74.5%]">
         <button className="bg-[#007179] mt-[3vh] border-[2vh] border-white text-white text-[5vh] py-[1vh] px-[7vh] rounded-[8vw]"
           onClick={() => { setShowPopUp(false); goToScreen('fifth', setGameStateRef); }}>
@@ -46,92 +48,91 @@ export default function FourthScreen({ sendAdminMessage }: BaseProps) {
         </button>
       </div>}
 
+      <div className="flex flex-col items-center gap-[2vh] justify-center min-h-screen drop-shadow-xl">
+        <div className={`flex items-center justify-center leading-none text-center bg-white text-[6vh]`}>
+          <h1 className="p-[1.5vh] px-[3vh]">{Math.floor(number1 / 10)}</h1>
+          <h1 className="p-[1.5vh] px-[3vh]">{number1 % 10}</h1>
+        </div>
 
+        <div className={`flex items-center justify-center leading-none text-center  text-[6vh]`}>
+          <h1 className="p-[1.5vh] px-[3vh]">x</h1>
+          <h1 className="p-[1.5vh] px-[3vh] bg-white">{number2}</h1>
+        </div>
 
-      <div className={`flex items-center justify-center leading-none text-center bg-white text-[6vh]`}>
-        <h1 className="p-[1.5vh] px-[3vh]">{Math.floor(number1 / 10)}</h1>
-        <h1 className="p-[1.5vh] px-[3vh]">{number1 % 10}</h1>
+        <div className="px-[15vh] h-0 border-t-[0.4vh] border-black"></div>
 
-      </div>
+        <div className="flex items-center justify-center gap-[2vh] ml-[15.5vh] leading-none text-center bg-white text-[5vh] p-[1.5vh]">
 
-      <div className={`flex items-center justify-center leading-none text-center  text-[6vh]`}>
-        <h1 className="p-[1.5vh] px-[3vh]">x</h1>
-        <h1 className="p-[1.5vh] px-[3vh] bg-white">{number2}</h1>
-      </div>
-
-      <div className="px-[15vh] h-0 border-t-[0.4vh] border-black"></div>
-
-      <div className="flex items-center justify-center gap-[2vh] ml-[15.5vh] leading-none text-center bg-white text-[5vh] p-[1.5vh]">
-
-        <NewInput
-          value={ans1}
-          correctValue={(Math.floor(number1 / 10) * 10 * number2).toString()}
-          onValueChange={(value) => setAns1(value)}
-          placeholder="?"
-          className="w-[16vh] text-white placeholder:text-white border-none outline-none p-[1vh]   text-center text-[5vh] bg-[#0095b7]"
-          ref={ans1Ref}
-          onCorrect={() => { setBlurAns2(false); ans2Ref.current?.focus(); sounds.right(); }}
-          onIncorrect={(attempt, correct) => {
-            sounds.wronginput();
-            sendAdminMessage('admin', `User has entered ${attempt} which is wrong for ${Math.floor(number1 / 10) * 10} x ${number2}, the answer is ${correct}, the question is ${number1} x ${number2} partial product, diagnose socratically with respect to user's current game state`);
-          }}
-        />
-        <div>{Math.floor(number1 / 10) * 10}</div>
-        <div>x</div>
-        <div>{number2}</div>
-      </div>
-
-      <div className={`flex items-center justify-center gap-[2vh] ml-[10vh] transition-all duration-500 ${blurAns2 ? 'blur-[0.5vh]' : ''}`}>
-        <div className="text-[7vh]">+</div>
-
-        <div className="flex items-center justify-center gap-[2vh] leading-none text-center bg-white text-[5vh] p-[1.5vh]">
           <NewInput
-            value={ans2}
-            correctValue={((number1 % 10) * number2).toString()}
-            onValueChange={(value) => setAns2(value)}
+            value={ans1}
+            correctValue={(Math.floor(number1 / 10) * 10 * number2).toString()}
+            onValueChange={(value) => setAns1(value)}
             placeholder="?"
-            className="w-[16vh] text-white placeholder:text-white border-none outline-none p-[1vh]   text-center text-[5vh] bg-[#c45500]"
-            ref={ans2Ref}
-            onCorrect={() => { 
-              setBlurAns3(false); 
-              ans3Ref.current?.focus();
-              sounds.right();
-              sendAdminMessage('agent', `Perfect, now that we have the partial products, let's add them`);
-            }}
+            className="w-[16vh] text-white placeholder:text-white border-none outline-none p-[1vh]   text-center text-[5vh] bg-[#0095b7]"
+            ref={ans1Ref}
+            onCorrect={() => { setBlurAns2(false); ans2Ref.current?.focus(); sounds.right(); }}
             onIncorrect={(attempt, correct) => {
               sounds.wronginput();
-              sendAdminMessage('admin', `User has entered ${attempt} which is wrong for ${(number1 % 10)} x ${number2}, the answer is ${correct}, the question is ${number1} x ${number2} partial product, diagnose socratically with respect to user's current game state`);
+              sendAdminMessage('admin', `User has entered ${attempt} which is wrong for ${Math.floor(number1 / 10) * 10} x ${number2}, the answer is ${correct}, the question is ${number1} x ${number2} partial product, diagnose socratically with respect to user's current game state`);
             }}
           />
-          <div className="pl-[2.5vh]">{number1 % 10}</div>
+          <div>{Math.floor(number1 / 10) * 10}</div>
           <div>x</div>
           <div>{number2}</div>
         </div>
-      </div>
 
-      <div className="px-[15vh] h-0 border-t-[0.4vh] border-black"></div>
+        <div className={`flex items-center justify-center gap-[2vh] ml-[10vh] transition-all duration-500 ${blurAns2 ? 'blur-[0.5vh]' : ''}`}>
+          <div className="text-[7vh]">+</div>
 
-      <div className={`flex items-center justify-center gap-[2vh] leading-none text-center bg-white text-[5vh] p-[1.5vh] transition-all duration-500 ${blurAns3 ? 'blur-[0.5vh]' : ''}`}>
-        <NewInput
-          value={ans3}
-          correctValue={(number1 * number2).toString()}
-          onValueChange={(value) => setAns3(value)}
-          placeholder="?"
-          className="w-[16vh] text-white placeholder:text-white border-none outline-none p-[1vh]   text-center text-[5vh] bg-[#5c9f00]"
-          ref={ans3Ref}
-          onCorrect={() => {
-            setShowSuccess(true);
-            sounds.right();
-            sendAdminMessage('agent', `Awesome, let's do some more.`);
-            setTimeout(() => {
-              setShowPopUp(true);
-            }, 4000);
-          }}
-          onIncorrect={(attempt, correct) => {
-            sounds.wronginput();
-            sendAdminMessage('admin', `User has entered wrong sum ${attempt}, the answer is ${correct}, the question is sum after partial product  ${number1} x ${number2}, diagnose socratically with respect to user's current game state`);
-          }}
-        />
+          <div className="flex items-center justify-center gap-[2vh] leading-none text-center bg-white text-[5vh] p-[1.5vh]">
+            <NewInput
+              value={ans2}
+              correctValue={((number1 % 10) * number2).toString()}
+              onValueChange={(value) => setAns2(value)}
+              placeholder="?"
+              className="w-[16vh] text-white placeholder:text-white border-none outline-none p-[1vh]   text-center text-[5vh] bg-[#c45500]"
+              ref={ans2Ref}
+              onCorrect={() => {
+                setBlurAns3(false);
+                ans3Ref.current?.focus();
+                sounds.right();
+                sendAdminMessage('agent', `Perfect, now that we have the partial products, let's add them`);
+              }}
+              onIncorrect={(attempt, correct) => {
+                sounds.wronginput();
+                sendAdminMessage('admin', `User has entered ${attempt} which is wrong for ${(number1 % 10)} x ${number2}, the answer is ${correct}, the question is ${number1} x ${number2} partial product, diagnose socratically with respect to user's current game state`);
+              }}
+            />
+            <div className="pl-[2.5vh]">{number1 % 10}</div>
+            <div>x</div>
+            <div>{number2}</div>
+          </div>
+        </div>
+
+        <div className="px-[15vh] h-0 border-t-[0.4vh] border-black"></div>
+
+        <div className={`flex items-center justify-center gap-[2vh] leading-none text-center bg-white text-[5vh] p-[1.5vh] transition-all duration-500 ${blurAns3 ? 'blur-[0.5vh]' : ''}`}>
+          <NewInput
+            value={ans3}
+            correctValue={(number1 * number2).toString()}
+            onValueChange={(value) => setAns3(value)}
+            placeholder="?"
+            className="w-[16vh] text-white placeholder:text-white border-none outline-none p-[1vh]   text-center text-[5vh] bg-[#5c9f00]"
+            ref={ans3Ref}
+            onCorrect={() => {
+              setShowSuccess(true);
+              sounds.right();
+              sendAdminMessage('agent', `Awesome, let's do some more.`);
+              setTimeout(() => {
+                setShowPopUp(true);
+              }, 4000);
+            }}
+            onIncorrect={(attempt, correct) => {
+              sounds.wronginput();
+              sendAdminMessage('admin', `User has entered wrong sum ${attempt}, the answer is ${correct}, the question is sum after partial product  ${number1} x ${number2}, diagnose socratically with respect to user's current game state`);
+            }}
+          />
+        </div>
       </div>
 
       {showSuccess && <SuccessAnimation />}
